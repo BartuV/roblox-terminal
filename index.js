54ca54ee533a08249ce17d34a5b059edf20ae047
BartuV <brkmn23@gmail.com>
2023-02-05 21:27:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const colors = require('colors');
+const app = express();
+const port = 5000;
+const prompt = require("prompt-sync")();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+colors.enable()
+let command = ''
+
+function betterOutput(string) {
+    return string.replace(/^.*?: /mgi, "");
+}
+
+function getInput(){
+    command = prompt("> ");
+    if (command === "exit") process.exit(1);  
+    return
+}
+
+function print(text){
+    console.log("\n"+text+"\n")
+}
+
+app.get("/", (req, res) => {
+    getInput();
+    res.send(command);
+});
+
+app.post("/", (req, res) => {
+    let successful = req.body.successful
+    let output = req.body.data
+
+    if (successful === "true"){
+        print(output.green)
+    }else{
+        print(betterOutput(output).red)
+    }
+
+    res.sendStatus(200)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
